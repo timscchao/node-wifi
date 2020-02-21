48e33603bbe69c351c4591ccfbbcf475e7ea8785
Tim Chao <tim.chao@ingics.com>
2020-02-21 17:32:59 +0800

    fix(linux-delete): delete connections by ssid matching
    The connection name did not always match the SSID in Linux Network
    Management, this change modify to delete connection by search the
    connections which match the specified SSID.
    
    Signed-off-by: Tim Chao <tim.chao@ingics.com>

--- src/linux-delete.js
+++ src/linux-delete.js
@@ -1,15 +1,97 @@
+var spawn = require('child_process').spawn;
 var execFile = require('child_process').execFile;
+var execFileSync = require('child_process').execFileSync;
 var env = require('./env');
 
-function deleteConnection(config, ap, callback) {
+function queryConnectionSsid(name, callback) {
+  var p1 = spawn('nmcli', ['connection', 'show', name], env);
+  var p2 = spawn('grep', ['802-11-wireless.ssid'], env);
+  var ssid = '';
+  p1.stdout.on('data', data => {
+    p2.stdin.write(data);
+  });
+  p1.on('close', code => {
+    p2.stdin.end();
+  });
+  p2.stdout.on('data', data => {
+    var fields = data.toString().split(/[ :\n]+/);
+    if (fields.length >= 2) {
+      ssid = fields[1];
+    }
+  });
+  p2.on('close', code => {
+    callback && callback(null, ssid);
+  });
+}
+
+function listConnectionsBySsid(ssid, callback) {
   var args = [];
+  args.push('--terse');
+  args.push('--fields');
+  args.push('name,uuid,type,device');
   args.push('connection');
-  args.push('delete');
-  args.push('id');
 
-  args.push(ap.ssid);
+  execFile('nmcli', args, env, function(err, result) {
+    if (err) {
+      callback && callback(err);
+      return;
+    }
+    var lines = result.split('\n');
+    var count = 0;
+    var connections = [];
+    lines.forEach(line => {
+      if (line != '' && line.includes(':')) {
+        var fields = line.replace(/\\:/g, '&&').split(':');
+        if (fields[2] === '802-11-wireless') {
+          queryConnectionSsid(fields[0], (err, conssid) => {
+            if (ssid === conssid) {
+              connections.push({
+                name: fields[0],
+                uuid: fields[1],
+                type: fields[2],
+                device: fields[3],
+                ssid: conssid
+              });
+            }
+            count++;
+            if (count === lines.length) {
+              callback && callback(null, connections);
+            }
+          });
+        } else {
+          count++;
+          if (count === lines.length) {
+            callback && callback(null, connections);
+          }
+        }
+      } else {
+        count++;
+        if (count === lines.length) {
+          callback && callback(null, connections);
+        }
+      }
+    });
+  });
+}
 
-  execFile('nmcli', args, env, function(err) {
+function deleteConnection(config, ap, callback) {
+  listConnectionsBySsid(ap.ssid, (err, connections) => {
+    var err = null;
+    connections.forEach(connection => {
+      if (err) {
+        return;
+      }
+      var args = [];
+      args.push('connection');
+      args.push('delete');
+      args.push('id');
+      args.push(connection.name);
+      try {
+        execFileSync('nmcli', args, env);
+      } catch (error) {
+        err = error.error;
+      }
+    });
     callback && callback(err);
   });
 }
